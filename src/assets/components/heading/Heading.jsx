c278fb8b55c48413047f85b5d0a54c19cbf087c0
ogunTee <adeniyioguntamu@gmail.com>
2023-11-14 14:03:06 +0100

    HEADING EDIT

--- src/assets/components/heading/Heading.jsx
+++ src/assets/components/heading/Heading.jsx
@@ -4,7 +4,7 @@ const Heading = () => {
   return (
     <div>
       <h3 className="mt-5 text-center">Welcome To Street Store.</h3>
-      <h1 className="mt-5 text-center">WE ARE GLAD YOU VISIT</h1>
+      <h1 className="mt-5 text-center">WE ARE GLAD YOU ARE HERE</h1>
     </div>
   );
 };
