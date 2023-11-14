a4c921bb6cbfa083ecfbbe8d9d7cba7f2f10b087
ogunTee <adeniyioguntamu@gmail.com>
2023-11-14 14:02:01 +0100

    heading edit

--- src/assets/components/heading/Heading.jsx
+++ src/assets/components/heading/Heading.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Heading = () => {
   return (
     <div>
-      <h3 className="mt-5 text-center">Welcome To Our Store.</h3>
+      <h3 className="mt-5 text-center">Welcome To Street Store.</h3>
       <h1 className="mt-5 text-center">WE ARE GLAD YOU VISIT</h1>
     </div>
   );
