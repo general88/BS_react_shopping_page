7078851a7dddfe8105781db43655ad06265f0a90
ogunTee <adeniyioguntamu@gmail.com>
2023-11-14 13:32:07 +0100

    fixing images bug

--- src/assets/components/cards/Cards.jsx
+++ src/assets/components/cards/Cards.jsx
@@ -9,7 +9,7 @@ const Cards = () => {
       <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 gap-4 justify-content-center mt-5 ">
         {shopItemsData.map((item) => (
           <Card key={item.id} className="col" style={{ width: "18rem" }}>
-            <Card.Img className="" variant="top" src={`./${item.img}`} />
+            <Card.Img className="" variant="top" src={item.img} />
             <Card.Body>
               <Card.Title>{item.productTitle}</Card.Title>
               <Card.Text>{item.desc}</Card.Text>
